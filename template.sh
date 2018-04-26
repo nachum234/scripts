0bd4cbd42bca707fabc9d5506ae3328d83ac8a64
Yossi Nachum <nachum234@gmail.com>
2018-04-26 10:02:39 +0300

    add status file function and exaple for using opts

--- template.sh
+++ template.sh
@@ -39,8 +39,39 @@ function err_handle
 	fi
 }
 ################################################################
+function write_status_file
+{
+  STATUS=$1
+  echo "TIMESTAMP=$(date +%s)" > ${STATUS_FILE}
+  echo "STATUS=${STATUS}" >> ${STATUS_FILE}
+  echo "OUTPUT=${OUTPUT}" >> ${STATUS_FILE}
+}
+################################################################
+function usage
+{
+  echo ${SCRIPT_NAME} [-s STATUS_FILE_PATH] command
+  exit 1
+}
+################################################################
 #
 # Main
 #
 ################################################################
+check_if_already_run
+while getopts ":s:" OPT; do
+  case ${OPT} in
+    s)
+      STATUS_FILE=${OPTARG}
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+CMD=$*
 
+if [ "x${CMD}" = "x" ]
+then
+  usage
+fi
